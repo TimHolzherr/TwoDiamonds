f67400bd7f1618368da9ebdfafee7de57c3908b8
TimHolzherr <tim_holzherr@bluewin.ch>
2021-05-24 16:03:53 +0200

    5 clones

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ function add(point, offsetX, offsetY) {
     }
 }
 
+function createDiamond(center, scale, color) {
+    var distortion = 1.5 
+    var left = add(center, -1 * scale, 0);
+    var top = add(center, 0, -1 * scale * distortion);
+    var right = add(center, scale, 0);
+    var bottom = add(center, 0, scale * distortion);
+    return `<path  id="svg_7" d="M ${left.x} ${left.y} L ${top.x} ${top.y} L ${right.x} ${right.y} L ${bottom.x} ${bottom.y} Z" fill="#${color}${color}${color}"/>`
+}
 
 const svgContainer = document.getElementById("svg");
 const controlContainer = document.getElementById("control")
@@ -28,23 +36,23 @@ function render() {
         y: Number(center2ySlider.value),
     }
     
-    var left1 = add(center1, -10, 0);
-    var top1 = add(center1, 0, -15);
-    var right1 = add(center1, 10, 0);
-    var bottom1 = add(center1, 0, 15);
+    var paths = "";
+    var scales = [30, 25, 20, 15, 10, 5]
+    var colors = ["00", "33", "66", "99", "bb", "ff"]
+    for (var index = 0; index <= 5; index++ ) {
+        paths += createDiamond(center1, scales[index], colors[index]);
+        paths += createDiamond(center2, scales[index], colors[index]);
+    }
     
-    var left2 = add(center2, -10, 0);
-    var top2 = add(center2, 0, -15);
-    var right2 = add(center2, 10, 0);
-    var bottom2 = add(center2, 0, 15);
+   
+
     
     var svgText = `
     <svg width="800" height="800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <g id="Layer_1">
      <title>Layer 1</title>
      <g stroke="null" id="svg_9">
-     <path  id="svg_7" d="M ${left1.x} ${left1.y} L ${top1.x} ${top1.y} L ${right1.x} ${right1.y} L ${bottom1.x} ${bottom1.y} Z" fill="#000000"/>
-     <path  id="svg_7" d="M ${left2.x} ${left2.y} L ${top2.x} ${top2.y} L ${right2.x} ${right2.y} L ${bottom2.x} ${bottom2.y} Z" fill="#000000"/>     
+     ${paths}     
      </g>
     </g>
     </svg>
